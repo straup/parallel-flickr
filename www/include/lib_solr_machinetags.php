a72dc624ff35762411ce60c45e035c920a2189de
straup <straup@gmail.com>
2011-11-14 17:53:57 +0100

    add explode function to lib_solr_machinetags

--- www/include/lib_solr_machinetags.php
+++ www/include/lib_solr_machinetags.php
@@ -2,6 +2,36 @@
 
 	#################################################################
 
+	# http://www.aaronland.info/talks/mw10_machinetags/#105
+
+	function solr_machinetags_explode($mt, $add_lazy8s=1){
+
+		list($ns, $rest) = explode(":", $mt, 2);
+		list($pred, $value) = explode("=", $rest, 2);
+
+		$parts = array(
+			"{$ns}:",
+			"{$ns}:{$pred}=",
+			"{$ns}:{$pred}={$value}",
+			"={$value}",
+			":{$pred}=",
+			":{$pred}={$value}",
+		);
+
+		if ($add_lazy8s){
+
+			$count = count($parts);
+
+			for ($i=0; $i < $count; $i++){
+				$parts[$i] = solr_machinetags_add_lazy8s($parts[$i]);
+			}
+		}
+
+		return $parts;
+	}
+
+	#################################################################
+
 	function solr_machinetags_add_lazy8s($str){
 		$str = preg_replace("/8/", "88", $str);
 		$str = preg_replace("/:/", "8c", $str);
