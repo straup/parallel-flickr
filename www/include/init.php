389880cdb6c592b716c132553456055e14b85cc1
straup <straup@gmail.com>
2011-11-17 17:30:29 +0100

    fix naming of site_disabled hooks; add hooks to set 503 headers

--- www/include/init.php
+++ www/include/init.php
@@ -165,7 +165,15 @@
 	loadlib('smarty');	# smarty comes next, since other libs register smarty modules
 	loadlib('utf8');	# make sure utf8/header stuff is present in case we need to take the site down
 
-	if (($GLOBALS['cfg']['disable_site']) && (! $this_is_shell)){
+	if (($GLOBALS['cfg']['site_disabled']) && (! $this_is_shell)){
+
+		header("HTTP/1.1 503 Service Temporarily Unavailable");
+		header("Status: 503 Service Temporarily Unavailable");
+
+		if ($retry = intval($GLOBALS['cfg']['site_disabled_retry_after'])){
+			header("Retry-After: {$retry}");
+		}
+
 		$smarty->display("page_site_disabled.txt");
 		exit();
 	}
