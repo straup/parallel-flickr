45f9e2205c33a3593e28dc49aa14fd98a59ca631
straup <straup@gmail.com>
2011-11-19 01:14:57 +0100

    stubbing out backfill tool for solr indexing

---
+++ bin/backfill_solr_index_photos.php
@@ -0,0 +1,28 @@
+<?php
+
+	$root = dirname(dirname(__FILE__));
+	ini_set("include_path", "{$root}/www:{$root}/www/include");
+
+	set_time_limit(0);
+
+	#
+
+	include("include/init.php");
+
+	loadlib("backfill");
+	loadlib("flickr_photos");
+	loadlib("flickr_photos_search");
+
+	if (! $GLOBALS['cfg']['enable_feature_solr']){
+		echo "search indexing is disabled, exiting";
+		exit();
+	}
+
+	function index_photo(&$row, $more=array()){
+		$photo = flickr_photos_get_by_id($row['id']);
+		$rsp = flickr_photos_search_index_photo($photo);
+	}
+
+	echo "HEY LOOK! THIS TOTALLY DOESN'T WORK YET";
+	exit();
+?>
