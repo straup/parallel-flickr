4fcc651de11164eef9053b462d56e89c67ec5e1c
straup <straup@gmail.com>
2011-11-25 19:20:57 +0100

    date wrangling on input/output

--- www/include/lib_flickr_photos_archives.php
+++ www/include/lib_flickr_photos_archives.php
@@ -16,7 +16,43 @@
 			'photo_owner' => $user['id'],
 		);
 
-		return flickr_photos_search_facet_dates($query, $facet, $start, $end, $gap, $more);
+		# this kind of thing will probably need to be moved in to
+		# one or more helper functions but it's not clear how or
+		# where yet... (20111125/straup)
+
+		if ($gap == "+1YEAR"){
+			list($yyyy, $ignore) = explode("-", $start, 2);
+			$start = "{$yyyy}-01-01 00:00:00";
+
+			list($yyyy, $ignore) = explode("-", $end, 2);
+			$end = "{$yyyy}-12-31 23:59:59";
+		}
+
+		$rsp = flickr_photos_search_facet_dates($query, $facet, $start, $end, $gap, $more);
+
+		if ($rsp['ok']){
+
+			$trimmed = array();
+
+			foreach ($rsp[$facet] as $dt => $count){
+
+				# see above inre: helper functions
+
+				$ts = strtotime($dt);
+				$fmt = "Y-m-d";
+
+				if ($gap == "+1YEAR"){
+					$fmt = "Y";
+				}
+
+				$dt = gmdate($fmt, $ts);
+				$trimmed[$dt] = $count;
+			}
+
+			$rsp[$facet] = $trimmed;
+		}
+
+		return $rsp;
 	}
 
 	#################################################################
