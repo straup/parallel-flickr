9918674faa2a112946807c97ec8056e823604f0b
straup <straup@gmail.com>
2011-12-04 18:23:23 +0100

    filter user faves by a specific user; add _path_alias function in lib_flickr_urls

--- www/flickr_faves_user.php
+++ www/flickr_faves_user.php
@@ -10,28 +10,38 @@
 
 	#
 
+	$viewer = $GLOBALS['cfg']['user'];
+
 	$flickr_user = flickr_users_get_by_url();
 	$owner = users_get_by_id($flickr_user['user_id']);
 
-	$is_own = ($owner['id'] == $GLOBALS['cfg']['user']['id']) ? 1 : 0;
+	$is_own = ($owner['id'] == $viewer['id']) ? 1 : 0;
 	$GLOBALS['smarty']->assign("is_own", $is_own);
 
 	#
 
 	$more = array(
-		'viewer_id' => $GLOBALS['cfg']['user']['id'],
+		'viewer_id' => $viewer['id'],
 		'page' => get_int32("page"),
 	);
 
 	if ($by_alias = get_str("by_alias")){
-		$more['by_owner'] = flickr_users_get_by_path_alias($by_alias);
+
+		if ($by_flickr_user = flickr_users_get_by_path_alias($by_alias)){
+			$more['by_owner'] = users_get_by_id($by_flickr_user['user_id']);
+		}
 	}
 
 	else if ($by_nsid = get_str("by_nsid")){
-		$more['by_owner'] = flickr_users_get_by_nsid($by_nsid);
+
+		if ($by_flickr_user = flickr_users_get_by_nsid($by_nsid)){
+			$more['by_owner'] = users_get_by_id($by_flickr_user['user_id']);
+		}
 	}
 
 	else {}
+
+	$by_owner = (isset($more['by_owner'])) ? $more['by_owner'] : null;
 
 	$faves = flickr_faves_for_user($owner, $more);
 	$photos = array();
@@ -42,7 +52,7 @@
 		$photo['owner'] = users_get_by_id($photo['user_id']);
 
 		# quick hack until perms are denormalized into the FlickrFaves table
-		$photo['canview'] = flickr_photos_permissions_can_view_photo($photo, $GLOBALS['cfg']['user']['id']);
+		$photo['canview'] = flickr_photos_permissions_can_view_photo($photo, $viewer['id']);
 
 		# going to leave this disable until I figure out what to
 		# do about reciprical contacts hoohah...
@@ -55,9 +65,11 @@
 	}
 
 	$GLOBALS['smarty']->assign_by_ref("owner", $owner);
+	$GLOBALS['smarty']->assign_by_ref("by_owner", $by_owner);
+
 	$GLOBALS['smarty']->assign_by_ref("photos", $photos);
 
-	$pagination_url = flickr_urls_faves_user($owner);
+	$pagination_url = flickr_urls_faves_user($owner, $by_owner);
 	$GLOBALS['smarty']->assign("pagination_url", $pagination_url);
 
 	$GLOBALS['smarty']->display("page_flickr_faves_user.txt");
