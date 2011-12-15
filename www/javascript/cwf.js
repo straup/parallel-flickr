a13ce682123e87c33627427ececd58a3ffff98fa
straup <straup@gmail.com>
2011-12-15 09:14:25 +0100

    set photo counts correctly

--- www/javascript/cwf.js
+++ www/javascript/cwf.js
@@ -301,7 +301,7 @@ function cwf_show_photo(index){
 	msg += " <span style=\"font-size:1.2em;font-weight:700;\">☆</span>  ";
 	msg += "<a href=\"http://www.flickr.com/photos/" + taken_by_nsid + "/\" target=\"_flickr\">" + taken_by_name + "</a><br />";
 
-	msg += "no. " + num + " of <span id=\"cwf_count_photos\">" + count_photos + "</span> faves";
+	msg += "no. " + num + " of <span id=\"cwf_count_photos\">" + photos.length + "</span> faves";
 
 	if (num > 1){
 		msg += " / <a href=\"#\" onclick=\"cwf_show_next_photo();return false;\">before</a>";
