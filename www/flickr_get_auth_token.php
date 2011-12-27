30b5621f5bced03fd715b202612219b9429743f5
straup <straup@gmail.com>
2011-12-27 19:37:37 +0100

    start fleshing out token swap

--- www/flickr_get_auth_token.php
+++ www/flickr_get_auth_token.php
@@ -4,25 +4,25 @@
 	loadlib("flickr_api");
 	loadlib("flickr_users");
 
-	error_404();
-
 	login_ensure_loggedin($_SERVER['REQUEST_URI']);
 
-	$flickr_user = flickr_users_get_by_user_id($GLOBALS['cfg']['user_id']);
+	$flickr_user = flickr_users_get_by_user_id($GLOBALS['cfg']['user']['id']);
 
 	$crumb_key = 'flickr_auth_token';
 	$GLOBALS['smarty']->assign("crumb_key", $crumb_key);
 
 	$perms = request_str("perms");
 
-	$perms_map = flickr_api_authtoken_perms_map('string keys');
+	$perms_map = flickr_api_authtoken_perms_map();
+	$perms_map_str = flickr_api_authtoken_perms_map('string keys');
+
 	$GLOBALS['smarty']->assign_by_ref("perms_map", $perms_map);
 
 	if (! $perms){
 		$perms = 'read';
 	}
 
-	elseif (! isset($perms_map[$perms])){
+	elseif (! isset($perms_map_str[$perms])){
 
 		$GLOBALS['smarty']->display("page_flickr_get_auth_token.txt");
 		exit();
@@ -32,13 +32,21 @@
 
 	if ($flickr_user['auth_token']){
 
-		if ($flickr_user['token_perms'] == $perms_map[$perms])){
-			# do something
+		# fix me: check for redir
+
+		if ($flickr_user['token_perms'] == $perms_map_str[$perms]){
+			header("location: /");
+			exit();
 		}
 
 		# confirm token perms change
 
 		if ((! crumb_check($crumb_key)) || (! post_str("confirm"))){
+
+			$old_perms = $perms_map[$flickr_user['token_perms']];
+
+			$GLOBALS['smarty']->assign("old_perms", $old_perms);
+			$GLOBALS['smarty']->assign("new_perms", $perms);
 
 			$GLOBALS['smarty']->display("page_flickr_get_auth_token.txt");
 			exit();
@@ -46,6 +54,8 @@
 
 	}
 
+dumper("WTF");
+exit;
 	# Build a URL with the perms for the auth token we're requesting
 	# and send the user there. Rocket science, I know...
 
