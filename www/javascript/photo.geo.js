9a5b0efbc58f1ab78573a3d27127ebc2b8030585
straup <straup@gmail.com>
2012-01-02 18:25:56 +0100

    finish chunking out stuff in to manageable functions

--- www/javascript/photo.geo.js
+++ www/javascript/photo.geo.js
@@ -1,34 +1,51 @@
-// TO DO: clean up markup and semantic groupings of the various options...
+// TO DO: pass down / read from PHP geo context map
+
+var photo_geo_ctx_map = {
+	0: 'in a place that doesn\'t matter (not defined)',
+	1: 'indoors',
+	2: 'outdoors'
+};
+
 
 function photo_geo_edit_meta(photo_id){
 
-	var old_ctx = $("#edit_geo").attr("geo:context");
-
-	// TO DO: pass down / read from PHP geo context map
-
-	var ctx_map = {
-		0: 'in a place that doesn\'t matter (not defined)',
-		1: 'indoors',
-		2: 'outdoors'
-	};
-
 	var html = '<div id="modal_geo">';
 	html += '<div id="photo_geo_status"></div>';
-	html += '<form id="photo_geo_update_context">';
 
+	html += _photo_geo_context_generate_html();
+	html += _photo_geo_corrections_generate_html();
+
+	html += '<div id="photo_geo_close"><a href="#" onclick="$.modal.close(); return false;">close</a></div>';
+	html += '</div>';
+
+	// http://www.ericmmartin.com/projects/simplemodal/
+
+	$.modal(html);
+
+	$("#photo_geo_corrections_fetch").click(_photo_geo_corrections_fetch_onclick);
+	$("#photo_geo_update_context").submit(_photo_geo_context_update_onsubmit);
+}
+
+function _photo_geo_context_generate_html(){
+
+	var old_ctx = $("#edit_geo").attr("geo:context");
+
+	var html = '<div id="photo_geo_context">';
 	html += '<h3>Edit the geo context for this photo</h3>';
+
+	html += '<form id="photo_geo_update_context">';
 	html += 'This photo was taken ';
 
 	html += '<select id="new_geocontext">';
 
-	for (i in ctx_map){
+	for (i in photo_geo_ctx_map){
 		html += '<option value="' + i + '"';
 
 		if (i == old_ctx){
 			html += ' selected="true" disabled="true"';
 		}
 
-		html += '>' + ctx_map[i] + '</option>';
+		html += '>' + photo_geo_ctx_map[i] + '</option>';
 	}
 
 	html += '</select>';
@@ -36,103 +53,102 @@ function photo_geo_edit_meta(photo_id){
 	html += '<input type="submit" value="UPDATE" />';
 	html += '</form>';
 
-	// corrections
+	html += '</div>';
+	return html;
+}
+
+function _photo_geo_corrections_generate_html(){
+
+	var html = '';
 
 	// FIX ME: make me less brittle...
 	var old_placename = $("#geo_placename a").html();
 
 	html += '<div id="photo_geo_corrections">';
 	html += '<h3>Edit the place name for this photo</h3>';
+
 	html += '<p>Flickr thinks this photo was taken in <q>';
 	html += old_placename;
 	html += '</q>. <a href="#" id="photo_geo_corrections_fetch">Fetch the list of alternate place names.</a>';
 	html += '</p>';
 
 	html += '</div>';
-
-	html += '</div>';
-	html += '<div id="photo_geo_close"><a href="#" onclick="$.modal.close(); return false;">close</a></div>';
-
-	// http://www.ericmmartin.com/projects/simplemodal/
-
-	$.modal(html);
-
-	$("#photo_geo_corrections_fetch").click(photo_geo_corrections_fetch);
-
-	$("#photo_geo_update_context").submit(function(){
-
-		var new_ctx = $("#new_geocontext");
-		new_ctx = new_ctx.val();
-
-		if (new_ctx == old_ctx){
-			alert("Hey! There's nothing to update!");
-			return;
-		}
-
-		var args = {
-			'method': 'flickr.photos.geo.setContext',
-			'photo_id': photo_id,
-			'context': new_ctx
-		};
-
-		var _onsuccess = function(rsp){
-
-			if (rsp['stat'] != 'ok'){
-				$("#photo_geo_status").html("Ack! There was a problem calling the Flickr API.");
-				return;
-			}
-
-			$("#edit_geo").attr("geo:context", rsp['context']);
-
-			var str_ctx = ctx_map[rsp['context']];
-
-			var taken = '';
-
-			if (rsp['context'] != 0){
-
-				var woeid = $("#edit_geo").attr("geo:woeid");
-				var url = places_url + woeid + '/' + str_ctx + '/';
-
-				taken = 'It was taken <a href="' + url + '">' + str_ctx + '</a>';
-			}
-
-			$("#geo_context").html(taken);
-
-			var msg = '<p>Success! The geo context for this photo has been updated and is now <strong>' + str_ctx + '</strong>.</p>';
-			$("#photo_geo_status").html(msg);
-		};
-
-		$.ajax({
-			'url': '/api/',
-			'type': 'POST',
-			'data': args,
-			'success': _onsuccess
-		});
-
-		$("#photo_geo_update_context").hide();
-		$("#photo_geo_status").html("Poking the Flickr API...");
-
-		return false;
-	});
+	return html;
 }
 
-function photo_geo_corrections_fetch(){
+function _photo_geo_context_update_onsubmit(){
 
-		var args = {
-			'method': 'flickr.photos.geo.possibleCorrections', 
-			'photo_id': photo_id,
-			'place_type': 'neighbourhood'
-		};
+	var old_ctx = $("#edit_geo").attr("geo:context");
 
-		$.ajax({
-			'url': '/api/',
-			'type': 'GET',
-			'data': args,
-			'success': _photo_geo_possible_corrections_onsuccess
-		});
+	var new_ctx = $("#new_geocontext");
+	new_ctx = new_ctx.val();
 
-		$("#photo_geo_corrections_fetch").hide();
-		$("#photo_geo_status").html("Fetching alternate place names...");
+	if (new_ctx == old_ctx){
+		alert("Hey! There's nothing to update!");
+		return;
+	}
+
+	var args = {
+		'method': 'flickr.photos.geo.setContext',
+		'photo_id': photo_id,
+		'context': new_ctx
+	};
+
+	$.ajax({
+		'url': '/api/',
+		'type': 'POST',
+		'data': args,
+		'success': _photo_geo_set_context_onsuccess
+	});
+
+	$("#photo_geo_update_context").hide();
+	$("#photo_geo_status").html("Poking the Flickr API...");
+
+	return false;
+}
+
+function _photo_geo_set_context_onsuccess(rsp){
+
+	if (rsp['stat'] != 'ok'){
+		$("#photo_geo_status").html("Ack! There was a problem calling the Flickr API.");
+		return;
+	}
+
+	$("#edit_geo").attr("geo:context", rsp['context']);
+
+	var str_ctx = photo_geo_ctx_map[rsp['context']];
+
+	var taken = '';
+
+	if (rsp['context'] != 0){
+		var woeid = $("#edit_geo").attr("geo:woeid");
+		var url = places_url + woeid + '/' + str_ctx + '/';
+		taken = 'It was taken <a href="' + url + '">' + str_ctx + '</a>';
+	}
+
+	$("#geo_context").html(taken);
+
+	var msg = '<p>Success! The geo context for this photo has been updated and is now <strong>' + str_ctx + '</strong>.</p>';
+	$("#photo_geo_status").html(msg);
+}
+
+function _photo_geo_corrections_fetch_onclick(){
+
+	var args = {
+		'method': 'flickr.photos.geo.possibleCorrections', 
+		'photo_id': photo_id,
+		'place_type': 'neighbourhood'
+	};
+
+	$.ajax({
+		'url': '/api/',
+		'type': 'GET',
+		'data': args,
+		'success': _photo_geo_possible_corrections_onsuccess
+	});
+
+	$("#photo_geo_corrections_fetch").hide();
+	$("#photo_geo_status").html("Fetching alternate place names...");
 }
 
 function _photo_geo_possible_corrections_onsuccess(rsp){
