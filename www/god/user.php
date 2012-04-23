e08bd3cdb36f4533c3f6e1ae78f50979429c3410
straup <straup@gmail.com>
2012-04-23 09:28:35 +0200

    list backups (on god page)

--- www/god/user.php
+++ www/god/user.php
@@ -4,6 +4,7 @@
 
 	loadlib("god");
 	loadlib("flickr_photos");
+	loadlib("flickr_backups");
 	loadlib("flickr_faves");
 	loadlib("flickr_users");
 	loadlib("flickr_api");
@@ -23,11 +24,17 @@
 		$count_photos = flickr_photos_count_for_user($user, array('viewer_id' => $user['id']));
 		$user['count_photos'] = $count_photos;
 
-		$count_faves = flickr_faves_count_for_user($user, array('viewer_id' => $user['id']));
-		$user['count_faves'] = $count_faves;
+		if ($flickr_user['auth_token']){
 
-		$perms_map = flickr_api_authtoken_perms_map();
-		$GLOBALS['smarty']->assign_by_ref("perms_map", $perms_map);
+			$backups = flickr_backups_for_user($user);
+			$user['backups'] = $backups;
+
+			$count_faves = flickr_faves_count_for_user($user, array('viewer_id' => $user['id']));
+			$user['count_faves'] = $count_faves;
+
+			$perms_map = flickr_api_authtoken_perms_map();
+			$GLOBALS['smarty']->assign_by_ref("perms_map", $perms_map);
+		}
 
 		$GLOBALS['smarty']->assign_by_ref("user", $user);
 		$GLOBALS['smarty']->assign_by_ref("flickr_user", $flickr_user);
