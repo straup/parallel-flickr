675ac68f1b228729b142303854b336d0d49f18d3
straup <straup@gmail.com>
2013-05-22 14:05:09 +0200

    move local upload stuff in to separate libs

---
+++ www/include/lib_photos_resize.php
@@ -0,0 +1,39 @@
+<?php
+
+	#################################################################
+
+	# Where $sz is the maximum dimension on either side
+
+	function photos_resize($src, $dest, $sz, $more=array()){
+
+		$im = imagecreatefromjpeg($src);
+		list($w, $h, $type, $attr) = getimagesize($src);
+
+		if ($w > $h){
+			$ratio = $sz / $w;
+			$width = $sz;
+			$height = $h * $ratio;
+		}
+
+		else {
+			$ratio = $sz / $h;
+			$height = $sz;
+			$width = $w * $ratio;
+		}
+
+		# dumper("W,H: {$w},{$h} W,H:{$width},{$height}");
+
+		$resized = imagecreatetruecolor($width, $height);
+
+		imagecopyresampled($resized, $im, 0, 0, 0, 0, $width, $height, $w, $h);
+
+		imagejpeg($resized, $dest);
+
+		return array(
+			'ok' = 1,			
+		);
+	}
+
+	#################################################################
+
+	# the end
