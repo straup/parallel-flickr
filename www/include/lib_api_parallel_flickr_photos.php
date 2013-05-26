caab5d301c94304f459785a30b125830d448c73a
straup <straup@gmail.com>
2013-05-26 04:46:49 +0200

    stub up PF upload API method

---
+++ www/include/lib_api_parallel_flickr_photos.php
@@ -0,0 +1,48 @@
+<?php
+
+	loadlib("photos_upload");
+	loadlib("flickr_photos_upload");
+
+	#################################################################
+
+	function api_flickr_photos_upload(){
+
+		if (! $GLOBALS['cfg']['enable_feature_uploads']){
+			api_output_error(999, "uploads are disabled");
+		}
+
+		if (! $_FILES['photo']){
+			api_output_error(999, "missing photo");
+		}
+
+		if ($_FILES['photo']['error']){
+			api_output_error(999, "server error: {$_FILES['photo']['error']}");
+		}
+
+		$file = $_FILES['photo']['tmp_name'];
+
+		# FIX ME: pull in title, etc.
+
+		$args = array();
+
+		if ($delivery == 'flickr'){
+			$rsp = flickr_photos_upload($GLOBALS['cfg']['user'], $file, $args);
+		}
+
+		else {
+			$rsp = photos_upload($GLOBALS['cfg']['user'], $file, $args);
+		}
+
+		unlink($file);
+
+		if (! $rsp['ok']){
+			api_output_error(999, $rsp['error']);
+		}
+
+		api_output_ok($rsp);
+		exit();
+	}
+
+	#################################################################
+
+	# the end
