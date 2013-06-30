becea9784e28146624b702e3d520908cead81ece
straup <straup@gmail.com>
2013-06-30 06:22:00 +0200

    storage_get_file to read exif data

--- www/include/lib_flickr_photos_exif.php
+++ www/include/lib_flickr_photos_exif.php
@@ -19,7 +19,7 @@
 
 	#################################################################
 
-	# TO DO: account for things stored on S3 (20130629/straup)
+	# OMG - please cache me (20130629/straup)
 
 	function flickr_photos_exif_read(&$photo){
 
@@ -35,17 +35,32 @@
 			return not_okay("not a JPEG photo");
 		}
 
-		if (! storage_file_exists($path, array('boolean' => 1))){
-			return not_okay("original photo not found");
+		# if (! storage_file_exists($path, array('boolean' => 1))){
+		# 	return not_okay("original photo not found");
+		# }
+
+		# This is absolutely not awesome but it's what required to 
+		# support the S3 stuff... (20130629/straup)
+
+		$rsp = storage_get_file($path);
+
+		if (! $rsp){
+			return $rsp;
 		}
 
-		# TO DO: make this work with the S3 stuff (20130629/straup)
-		# can this read things with URI schemes ?
+		$tmpdir = sys_get_temp_dir();
+		$tmpfile = tempnam($tmpdir, "exif-{$photo['id']}");
+
+		$fh = fopen($tmpfile, 'w');
+		fwrite($fh, stream_get_contents($rsp['fh']));
+		fclose($fh);
+
+		$exif = exif_read_data($tmpfile);
+		unlink($tmpfile);
 
 		# abs_path is temporary (see above)
-		$path = flickr_photos_path($photo, array('size' => 'o', 'abs_path' => 1));
-
-		$exif = exif_read_data($path);
+		# $path = flickr_photos_path($photo, array('size' => 'o', 'abs_path' => 1));
+		# $exif = exif_read_data($path);
 
 		if (! $exif){
 			return not_okay("failed to read EXIF data");
